2fecfe68273a7b886d14cfddfad9a05fc269271e
Halavvapatil <Halavvapatil>
2024-08-14 15:58:33 +0530

    we

---
+++ snow1.sql
@@ -0,0 +1,37 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      DATA MODEL
+--
+-- Date Created : Wednesday, August 14, 2024 15:58:33
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE HYBRID TABLE Entity1(
+    id1    CHAR(10),
+    id2    CHAR(10),
+    PRIMARY KEY (id1) NOT DEFERRABLE 
+)
+;
+
+
+
+-- 
+-- TABLE: Entity2 
+--
+
+CREATE HYBRID TABLE Entity2(
+    id3    CHAR(10),
+    id4    CHAR(10),
+    id1    CHAR(10)    NOT NULL,
+    PRIMARY KEY (id1) NOT DEFERRABLE , 
+    FOREIGN KEY (id1)
+    REFERENCES Entity1(id1)
+)
+;
+
+
+
