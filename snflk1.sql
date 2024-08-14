cfa83f4b0b354d9642d84d49fd17f9812c4df89e
Halavvapatil <Halavvapatil>
2024-08-14 15:24:58 +0530

    sa

---
+++ snflk1.sql
@@ -0,0 +1,53 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      PKMODEL.dm1
+--
+-- Date Created : Wednesday, August 14, 2024 15:24:58
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE TABLE Entity1(
+    col8    CHAR(10),
+    col9    CHAR(10)
+)
+;
+
+
+
+-- 
+-- TABLE: TESTER12 
+--
+
+CREATE TABLE TESTER12(
+    TEST    VARCHAR(10)    NOT NULL,
+    COL1    CHAR(10),
+    col2    VARCHAR(10)    NOT NULL,
+    col3    VARCHAR(10)    NOT NULL,
+    PRIMARY KEY (col2) NOT DEFERRABLE 
+)
+;
+
+
+
+-- 
+-- TABLE: TABLE46 
+--
+
+CREATE TABLE TABLE46(
+    col4NEW    VARCHAR(10)    NOT NULL,
+    col5TES    VARCHAR(10)    NOT NULL,
+    col6       VARCHAR(10)    NOT NULL,
+    col2       VARCHAR(10)    NOT NULL,
+    PRIMARY KEY (col2) NOT DEFERRABLE , 
+    FOREIGN KEY (col2)
+    REFERENCES TESTER12(col2),
+
+)
+;
+
+
+
